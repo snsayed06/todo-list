13ac3cd2899e03e15770eae9d6c2457dd8864eb3
Sohail Sayed <snsayed06@gmail.com>
2024-04-09 22:00:20 -0400

    add autocomplete box

--- src/components/AutoComplete/AutoComplete.js
+++ src/components/AutoComplete/AutoComplete.js
@@ -1,9 +1,67 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import data from "./Data";
 
 const AutoComplete = () => {
-  return (
-    <div>AutoComplete</div>
-  )
-}
+  const [input, setInput] = useState("");
+  const [selected, setSelected] = useState([]);
+  const [search, setSearched] = useState([]);
+  const countries = data;
 
-export default AutoComplete
+  useEffect(() => {
+    const searchedCountry = countries.filter((country) =>
+      country.toLowerCase().includes(input.toLowerCase())
+    );
+    setSearched([...searchedCountry]);
+  }, [input]);
+
+  const inputHandler = (e) => {
+    setInput(e.target.value);
+  };
+
+  const selectHandler = (word) => {
+    if (!selected.includes(word)) {
+      setSelected([...selected, word]);
+      setInput('')
+    }
+  };
+
+  const removeSelectHandler = (word) => {
+    const remove = selected.filter((country) => word !== country);
+    setSelected([...remove]);
+  };
+
+  return (
+    <div>
+      {selected && (
+        <div>
+          {selected.map((word, index) => {
+            return (
+              <span onClick={() => removeSelectHandler(word)} key={index}>
+                {word}
+              </span>
+            );
+          })}
+        </div>
+      )}
+
+      <div>
+        <input type='text' value={input} onChange={(e) => inputHandler(e)} />
+      </div>
+
+      {input && (
+        <div>
+          {input.length &&
+            search.map((search, index) => {
+              return (
+                <p onClick={() => selectHandler(search)} key={index}>
+                  {search}
+                </p>
+              );
+            })}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default AutoComplete;
