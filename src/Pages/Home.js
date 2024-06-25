dde6faf531f8f2f16726e212640612f5af99cee7
Nauman Sayed <nsayed1@asu.edu>
2024-06-26 00:51:17 +0530

    new change

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,5 +1,5 @@
 const Home = () => {
-  return <h1 className="welcome_message">Hi, welcome to my world</h1>;
+  return <h1 className="dashboard_message">Hi, welcome to my world</h1>;
 };
 
 export default Home;
